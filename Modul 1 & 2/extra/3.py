6c7113b6291ebc522c2fde26ba121bb2fc83e9c7
NotHydra <rizky.irswanda115@gmail.com>
2023-08-25 20:08:33 +0800

    feat: 3 overhaul

---
+++ Modul 1 & 2/extra/3.py
@@ -0,0 +1,63 @@
+from os import system
+
+
+class Dependency:
+    textLength = 50
+    textFill = "="
+
+
+class Utility:
+    def clear() -> None:
+        system("cls")
+
+    def printFormat(text: str = "") -> None:
+        print(text.center(Dependency.textLength, Dependency.textFill))
+
+
+class Program:
+    def main() -> None:
+        programIsRunning = True
+        while programIsRunning:
+            try:
+                Utility.printFormat("Salary Calculator")
+
+                salaryPerHour = float(input("Salary Per Hour ($): "))
+                daysOfWorkPerWeek = int(input("Days Of Work Per Week: "))
+                if not (1 <= daysOfWorkPerWeek <= 7):
+                    raise Exception()
+
+                Utility.printFormat()
+                print(
+                    f"Total Weekly Salary ({str(daysOfWorkPerWeek) + ' Day' if daysOfWorkPerWeek == 1 else str(daysOfWorkPerWeek) + ' Days'}): ${salaryPerHour * 8 * daysOfWorkPerWeek}"
+                )
+                Utility.printFormat()
+
+            except:
+                Utility.printFormat()
+                print("Input Not Valid")
+                Utility.printFormat()
+
+            optionIsValid = False
+            while not optionIsValid:
+                option = input("Calculate Again? (Y/N): ")
+                if option == "Y":
+                    optionIsValid = True
+                    Utility.clear()
+
+                elif option == "N":
+                    Utility.printFormat()
+                    print("Thank You For Using Our Program")
+                    Utility.printFormat()
+
+                    programIsRunning = False
+                    optionIsValid = True
+
+                else:
+                    Utility.printFormat()
+                    print("Input Not Valid")
+                    Utility.printFormat()
+
+        input()
+
+
+Program.main()
