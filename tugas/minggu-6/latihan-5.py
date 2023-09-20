baea37a7bd2d6f432170151db214df6dec3876f4
NotHydra <rizky.irswanda115@gmail.com>
2023-09-20 22:09:05 +0800

    feat: multiple random testcases

--- tugas/minggu-6/latihan-5.py
+++ tugas/minggu-6/latihan-5.py
@@ -1,73 +1,43 @@
+from random import sample
+
+
 def debug(*text, active=False):
     if active:
         print(*text)
 
 
-caseList = [
-    {
-        "input": [5, 4, 8, 7, 2, 10, 1, 6, 3, 9],
-        "output": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    },
-    {
-        "input": [4, 3, 5, 1, 2],
-        "output": [1, 2, 3, 4, 5],
-    },
-]
+caseList = [sample(range(1, 500), 100) for _ in range(1, 50)]
 
-for caseDictionary in caseList:
-    inputList = caseDictionary["input"].copy()
+for caseObject in caseList:
+    sortedList = caseObject.copy()
 
     while True:
         switchCount = 0
 
         i = 1
-        while i < len(inputList):
-            debug(inputList)
+        while i < len(sortedList):
+            debug(sortedList)
 
-            tempPrevious = inputList[i - 1]
-            tempCurrent = inputList[i]
+            tempPrevious = sortedList[i - 1]
+            tempCurrent = sortedList[i]
 
             debug(i)
             debug(tempPrevious, tempCurrent)
 
             if tempPrevious > tempCurrent:
-                inputList[i] = tempPrevious
-                inputList[i - 1] = tempCurrent
+                sortedList[i] = tempPrevious
+                sortedList[i - 1] = tempCurrent
 
                 switchCount += 1
 
-            debug(inputList)
+            debug(sortedList)
             debug()
 
             i += 1
 
-        # i = len(inputList) - 2
-        # while i >= 0:
-        #     debug(inputList)
-
-        #     tempPrevious = inputList[i + 1]
-        #     tempCurrent = inputList[i]
-
-        #     debug(i)
-        #     debug(tempPrevious, tempCurrent)
-
-        #     if tempPrevious < tempCurrent:
-        #         inputList[i + 1] = tempCurrent
-        #         inputList[i] = tempPrevious
-
-        #         switchCount += 1
-
-        #     debug(inputList)
-        #     debug()
-
-        #     i -= 1
-
         if switchCount == 0:
             break
 
-    print(
-        caseDictionary["input"],
-        caseDictionary["output"],
-        inputList,
-        caseDictionary["output"] == inputList,
-    )
+    print(caseObject)
+    print(sortedList)
+    print()
