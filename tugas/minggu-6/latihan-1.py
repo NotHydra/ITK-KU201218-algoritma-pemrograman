15436dd2a201def21a5eab84b9746ad0b89cc604
NotHydra <rizky.irswanda115@gmail.com>
2023-09-20 22:23:47 +0800

    feat: random case

--- tugas/minggu-6/latihan-1.py
+++ tugas/minggu-6/latihan-1.py
@@ -1,7 +1,11 @@
-caseList = list(range(1, 6))
+from random import randint, sample
+
+# caseList = [randint(1, 10) for _ in range(1, 6)]
+caseList = sample(range(1, 10), k=5)
 
 total = 1
 for caseObject in caseList:
     total = total * caseObject
 
+print(caseList)
 print(total)
